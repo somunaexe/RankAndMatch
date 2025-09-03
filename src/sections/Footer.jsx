449c0ef1fe4a20600a2faff708d7cdcfd2d568b9
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2025-09-03 17:52:26 +0100

    4

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,8 +6,7 @@ const Footer = () => {
             <p>|</p>
             <p>Privacy Policy</p>
         </div>
-        <p className="text-white-500">Built with React <img src="/assets/react.svg" alt="react" className="w-5 h-5 inline-block" />, Javascript <img src="/assets/javascript.svg" alt="javascript" className="w-5 h-5 inline-block" />, Node.js <img src="/assets/node.png" alt="node" className="w-5 h-5 inline-block" />, Three.js <img src="/assets/three.png" alt="threejs" className="w-5 h-5 inline-block" />, Tailwind CSS <img src="/assets/tailwindcss.png" alt="tailwind" className="w-5 h-5 inline-block" />, AWS <img src="/assets/aws-light.png" alt="aws-light" className="w-5 h-5 inline-block" />, and Git <img src="/assets/git.png" alt="git" className="w-5 h-5 inline-block" />.</p>
-        <p className="text-white-500">© 2024 Somuna. All rights reserved.</p>
+        <p className="text-white-500">© 2025 Rank and Match. All rights reserved.</p>
     </footer>
   )
 }
