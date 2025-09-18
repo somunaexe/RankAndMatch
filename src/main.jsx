92c1196ff61a07702e44504582e2e6dbdd7f34da
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2025-09-18 02:08:15 +0000

    test fallback route

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import Admin from './admin/Admin.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import ConsentsForm from './sections/ConsentForm.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         {/* Page for customers */}
-        <Route path="/" element={<App />}/>
+        <Route path="/" element={<App />} />
+
+        {/* Fallback / Default Route */}
+        <Route path="*" element={<App />} />
+
+        {/* Page for casted members */}
+        {/* <Route path="consent" element={<ConsentsForm />} /> */}
 
         {/* ADMIN Pages for Somuna and Wunmi */}
         <Route path="/admin/*" element={<Admin />} />
