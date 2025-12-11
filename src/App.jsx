8654d25e8c986485dcec535109d619cc957f4302
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2025-12-11 03:05:32 +0000

    added gradient and cherry blossoms

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import Footer from './sections/Footer'
 // import Experience from './sections/Experience'
 const App = () => {
   return (
-    <main className='max-w-7xl mx-auto'>
+    <main className="max-w-7xl mx-auto">
+      <div className="absolute bg-[url('/assets/cherry.gif')] w-1/2 h-1/2 bg-cover bg-no-repeat" ></div>
       <Navbar navLinks={navLinks} admin={false}/>
       {/* <Hero /> */}
       {/* <About /> */}
